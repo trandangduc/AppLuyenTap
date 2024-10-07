ec9317a57868229c8aa035b48c972c9e3631abe4
Tran Duc <kienpro0987@gmail.com>
2024-10-07 08:06:10 +0700

    tao button

--- app/src/main/java/com/example/appluyentap/MainActivity.kt
+++ app/src/main/java/com/example/appluyentap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.appluyentap
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        val button : Button = findViewById(R.id.button)
     }
 }
