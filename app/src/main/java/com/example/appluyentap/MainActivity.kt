62dff787bdc715455354c7b02e1822f5da08484c
TaiGioTai <103432856+kirito215235325324@users.noreply.github.com>
2024-10-12 15:22:41 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/appluyentap/MainActivity.kt
+++ app/src/main/java/com/example/appluyentap/MainActivity.kt
@@ -15,35 +15,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        // Thiết lập lắng nghe sự kiện click cho các nút
-        val button1 = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
-        val button4 = findViewById<Button>(R.id.button4)
-
-        // Chuyển qua trang Ít vận động
-        button1.setOnClickListener {
-            val intent = Intent(this, ActivityItVanDong::class.java)
-            startActivity(intent)
-        }
-
-        // Chuyển qua trang Hơi tích cực
-        button2.setOnClickListener {
-            val intent = Intent(this, ActivityHoiTichCuc::class.java)
-            startActivity(intent)
-        }
-
-        // Chuyển qua trang Tích cực vừa phải
-        button3.setOnClickListener {
-            val intent = Intent(this, ActivityTichCucVuaPhai::class.java)
-            startActivity(intent)
-        }
-
-        // Chuyển qua trang Rất tích cực
-        button4.setOnClickListener {
-            val intent = Intent(this, ActivityRatTichCuc::class.java)
-            startActivity(intent)
-        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
