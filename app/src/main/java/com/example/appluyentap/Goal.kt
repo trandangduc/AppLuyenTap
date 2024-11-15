213c72b8aa4be12df2b360af2bac21450a656967
anhtai <laciaisthebest@gmail.com>
2024-11-15 17:52:07 +0700

    nopbaiii

--- app/src/main/java/com/example/appluyentap/Goal.kt
+++ app/src/main/java/com/example/appluyentap/Goal.kt
@@ -1,32 +1,20 @@
 package com.example.appluyentap
 
-import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Targetpage : AppCompatActivity() {
+class Goal : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_targetpage)
-
+        setContentView(R.layout.activity_goal)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Tìm nút Back trong layout
-        val buttonBack: ImageButton = findViewById(R.id.Targetback) // Đổi Button thành ImageButton
-
-        // Đặt sự kiện click cho nút Back
-        buttonBack.setOnClickListener {
-            finish() // Đóng Activity hiện tại và quay trở lại Activity trước đó
         }
     }
 }
